b5d4160d39b50f8a0793ad93b7fa27588e5f2a74
adrianschneider94 <post@adrian-schneider.de>
2023-07-24 12:36:14 +0200

    WIP

--- src/components/Grid/grid.tsx
+++ src/components/Grid/grid.tsx
@@ -1,98 +1,34 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useCallback, useRef} from 'react'
 import {ManipulateType} from 'dayjs'
 
-import {useIntervalCalculator} from '../../hooks/timeIntervals'
+import {defaultUnits, useIntervalCallback} from '../../hooks/timeIntervals'
 import {animated, to} from '@react-spring/web'
 import {round} from '../../functions/round'
-import {
-    useCanvasHeight,
-    useCanvasStore,
-    useCanvasStoreApi, useCanvasWidth,
-    useTimePerPixel,
-    useTimeStart
-} from '../Canvas/store'
-
-import {useAnimationFrame} from "../../hooks/animationFrame"
-import {IntervalToMs} from "../../functions/units"
+import {useCanvasHeight, useTimePerPixel, useTimeStart} from '../Canvas/store'
+import {Interval} from "../../functions/intervalFactory"
 
 
-function logRound(value: number, base = 2) {
-    return base ** (Math.round(Math.log(value) / Math.log(base)))
-}
-
-function roundTo(value: number, size: number) {
-    return size * Math.round(value / size)
-}
-
-const base = 1.2
-const factor = 0.7
-
-type NewGridProps = {
+type GridProps = {
     units?: [number, ManipulateType][]
     minWidth?: number
     n?: number
 }
 
 
-const defaultUnits: [number, ManipulateType][] = [
-    [100, 'years'],
-    [10, 'years'],
-    [1, 'year'],
-    [3, 'months'],
-    [1, 'month'],
-    [1, 'week'],
-    [1, 'day'],
-    [4, 'hours'],
-    [1, 'hour'],
-    [15, 'minutes'],
-    [1, 'minute'],
-    [15, 'seconds'],
-    [1, 'second'],
-    [100, 'milliseconds'],
-    [10, 'milliseconds'],
-    [1, 'milliseconds']
-]
-
-export const Grid: React.FC<NewGridProps> = ({units = defaultUnits, n = 150, minWidth = 20}) => {
-    let {createIntervals} = useIntervalCalculator()
-    let timeStartSpring = useTimeStart()
+export const Grid: React.FC<GridProps> = ({units = defaultUnits, n = 300, minWidth = 30}) => {
     let timePerPixelSpring = useTimePerPixel()
-    let canvasWidthSpring = useCanvasWidth()
+    let timeStartSpring = useTimeStart()
     let canvasHeight = useCanvasHeight()
 
-    let timeFrom = useRef(0)
-    let timeTo = useRef(0)
-
     let linePositions = useRef<number[]>([])
-    useAnimationFrame(async () => {
-        let timeStart = timeStartSpring.get()
-        let timePerPixel = timePerPixelSpring.get()
-        let canvasWidth = canvasWidthSpring.get()
+    let onIntervalsChange = useCallback((intervals: [number, Interval][][]) => {
+        let timePerPixel = timePerPixelSpring.goal
+        let filteredResults = intervals?.map(entry => entry?.filter(x => (x[1].end - x[1].start) / timePerPixel > minWidth))
+        let beginnings = filteredResults?.map(entry => entry?.map(x => x[1].start))
+        linePositions.current = beginnings.reduce((cumulated, next) => [...cumulated, ...next], [])
+    }, [minWidth, timePerPixelSpring])
 
-        let timeEnd = timeStart + timePerPixel * canvasWidth
-        let timeWidth = timeEnd - timeStart
-
-        const quantization = logRound(timeWidth, base)
-        const roundedTimeStart = roundTo(timeStart, quantization)
-        const roundedTimeEnd = roundTo(timeEnd, quantization)
-
-        let newFrom = roundedTimeStart - factor * quantization
-        let newTo = roundedTimeEnd + factor * quantization
-
-        if (timeFrom.current !== newFrom || timeTo.current !== newTo) {
-            timeFrom.current = newFrom
-            timeTo.current = newTo
-
-            let unitsToCalculate = units.filter(([amount, unit]) => amount * IntervalToMs[unit] / timePerPixel > minWidth)
-            let lowerBound = newFrom - (newTo - newFrom)
-            let upperBound = newTo + (newTo - newFrom)
-            let results = await Promise.all(unitsToCalculate.map(async ([amount, unit]) => await createIntervals(lowerBound, upperBound, amount, unit)))
-            results = results.filter(x => !!x)
-            let filteredResults = results?.map(entry => entry?.filter(x => (x[1].end - x[1].start) / timePerPixel > minWidth))
-            let beginnings = filteredResults?.map(entry => entry?.map(x => x[1].start))
-            linePositions.current = beginnings.reduce((cumulated, next) => [...cumulated, ...next], []).sort((a, b) => Math.abs(a - (newFrom + newTo) / 2) - Math.abs(b - (newFrom + newTo) / 2))
-        }
-    })
+    useIntervalCallback(onIntervalsChange, units, minWidth)
 
 
     return <g>
@@ -105,7 +41,7 @@ export const Grid: React.FC<NewGridProps> = ({units = defaultUnits, n = 150, min
                 x2={to(([timeStartSpring, timePerPixelSpring]), (timeStart, timePerPixel) => linePositions.current?.[index] ? round((linePositions.current?.[index] - timeStart) / timePerPixel) : 0)}
                 y1={0}
                 y2={canvasHeight.to(canvasHeight => round(canvasHeight))}
-                stroke={'rgba(0, 0, 0, 0.2)'} />
+                stroke={'rgba(0, 0, 0, 0.2)'}/>
         })}
     </g>
 }
--- src/hooks/timeIntervals.ts
+++ src/hooks/timeIntervals.ts
@@ -1,13 +1,33 @@
-import {OpUnitType} from 'dayjs'
-import {useCallback} from 'react'
+import {ManipulateType, OpUnitType} from 'dayjs'
+import {useCallback, useRef} from 'react'
 import {RpcProvider} from 'worker-rpc'
 import {Interval} from '../functions/intervalFactory'
+import {useCanvasWidth, useTimePerPixel, useTimeStart} from "../components/Canvas/store"
+import {useAnimationFrame} from "./animationFrame"
+import {IntervalToMs} from "../functions/units"
 
 let workerSingleton: Worker | undefined = undefined
 let rpcProviderSingleton: RpcProvider | undefined = undefined
 
 const workerPath = 'createIntervals.js'
-
+export const defaultUnits: [number, ManipulateType][] = [
+    [100, 'years'],
+    [10, 'years'],
+    [1, 'year'],
+    [3, 'months'],
+    [1, 'month'],
+    [1, 'week'],
+    [1, 'day'],
+    [4, 'hours'],
+    [1, 'hour'],
+    [15, 'minutes'],
+    [1, 'minute'],
+    [15, 'seconds'],
+    [1, 'second'],
+    [100, 'milliseconds'],
+    [10, 'milliseconds'],
+    [1, 'milliseconds']
+]
 export const useIntervalCalculator = () => {
     if (!workerSingleton) {
         workerSingleton = new Worker(workerPath)
@@ -41,4 +61,55 @@ export const useIntervalCalculator = () => {
     }, [rpcProvider])
 
     return {rpcProvider, createIntervals}
+}
+
+function logRound(value: number, base = 2) {
+    return base ** (Math.round(Math.log(value) / Math.log(base)))
+}
+
+function roundTo(value: number, size: number) {
+    return size * Math.round(value / size)
+}
+
+const base = 1.1
+const factor = 0.1
+
+export const useIntervalCallback = (onChange: (_intervals: [number, Interval][][]) => unknown, units = defaultUnits, minWidth = 30) => {
+    let {createIntervals} = useIntervalCalculator()
+    let timeStartSpring = useTimeStart()
+    let timePerPixelSpring = useTimePerPixel()
+    let canvasWidthSpring = useCanvasWidth()
+
+    let timeFrom = useRef(0)
+    let timeTo = useRef(0)
+
+    let intervals = useRef<[number, Interval][][]>([])
+    useAnimationFrame(async () => {
+        let timeStart = timeStartSpring.get()
+        let timePerPixel = timePerPixelSpring.get()
+        let timePerPixelGoal = timePerPixelSpring.goal
+        let canvasWidth = canvasWidthSpring.get()
+
+        let timeEnd = timeStart + timePerPixel * canvasWidth
+        let timeWidth = timeEnd - timeStart
+
+        const quantization = logRound(timeWidth, base)
+        const roundedTimeStart = roundTo(timeStart, quantization)
+        const roundedTimeEnd = roundTo(timeEnd, quantization)
+
+        let newFrom = roundedTimeStart - factor * quantization
+        let newTo = roundedTimeEnd + factor * quantization
+
+        if (timeFrom.current !== newFrom || timeTo.current !== newTo) {
+            timeFrom.current = newFrom
+            timeTo.current = newTo
+
+            let unitsToCalculate = units.filter(([amount, unit]) => amount * IntervalToMs[unit] / timePerPixelGoal > minWidth)
+            let lowerBound = newFrom - (newTo - newFrom)
+            let upperBound = newTo + (newTo - newFrom)
+            let results = await Promise.all(unitsToCalculate.map(async ([amount, unit]) => await createIntervals(lowerBound, upperBound, amount, unit)))
+            onChange(results.filter(x => !!x))
+        }
+    })
+    return intervals.current
 }
